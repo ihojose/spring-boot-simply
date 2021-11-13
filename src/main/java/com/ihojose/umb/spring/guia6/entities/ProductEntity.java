5959684f11761df6241f0acca125f823ecbb7d5b
Jose Buelvas <www.iho_jose@hotmail.com>
2021-11-13 14:44:06 -0500

    Update ProductEntity.java

--- src/main/java/com/ihojose/umb/spring/guia6/entities/ProductEntity.java
+++ src/main/java/com/ihojose/umb/spring/guia6/entities/ProductEntity.java
@@ -5,17 +5,6 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 
-/**
- * Copyright (c) 2021 Banco de Bogotá. All Rights Reserved.
- * <p>
- * Guia6 was developed by Core Banking (Gerencia de Desarrollo)
- * <p>
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * proprietary and confidential. For use this code you need to contact to
- * Banco de Bogotá and request exclusive use permission.
- * <p>
- * This file was write by Jose Buelvas <jbuelva@bancodebogota.com.co>.
- */
 @Getter
 @Setter
 @ToString
