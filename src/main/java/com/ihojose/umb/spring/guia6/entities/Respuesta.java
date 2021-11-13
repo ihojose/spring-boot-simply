36eae1b5a84dd8262dbcf40a4b4d6075a33ce902
Jose Buelvas <www.iho_jose@hotmail.com>
2021-11-13 14:44:23 -0500

    Update Respuesta.java

--- src/main/java/com/ihojose/umb/spring/guia6/entities/Respuesta.java
+++ src/main/java/com/ihojose/umb/spring/guia6/entities/Respuesta.java
@@ -2,17 +2,6 @@ package com.ihojose.umb.spring.guia6.entities;
 
 import lombok.*;
 
-/**
- * Copyright (c) 2021 Banco de Bogotá. All Rights Reserved.
- * <p>
- * Guia6 was developed by Core Banking (Gerencia de Desarrollo)
- * <p>
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * proprietary and confidential. For use this code you need to contact to
- * Banco de Bogotá and request exclusive use permission.
- * <p>
- * This file was write by Jose Buelvas <jbuelva@bancodebogota.com.co>.
- */
 @Getter
 @Setter
 @NoArgsConstructor
