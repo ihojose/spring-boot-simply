8f491d34bf849d374d2f5d821a14ab38a963fdcb
Jose Buelvas <www.iho_jose@hotmail.com>
2021-11-13 14:43:42 -0500

    Update ProductController.java

--- src/main/java/com/ihojose/umb/spring/guia6/controller/ProductController.java
+++ src/main/java/com/ihojose/umb/spring/guia6/controller/ProductController.java
@@ -12,17 +12,6 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Copyright (c) 2021 Banco de Bogotá. All Rights Reserved.
- * <p>
- * Guia6 was developed by Core Banking (Gerencia de Desarrollo)
- * <p>
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * proprietary and confidential. For use this code you need to contact to
- * Banco de Bogotá and request exclusive use permission.
- * <p>
- * This file was write by Jose Buelvas <jbuelva@bancodebogota.com.co>.
- */
 @RestController
 @RequestMapping("api")
 public class ProductController {
